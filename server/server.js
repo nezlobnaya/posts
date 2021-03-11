3bc5472ee6f6fc4b7bd0fdfc7762b288dff72c42
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-03-10 21:35:31 -0500

    Promise changed using try/catch, nodemon activated

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config()
 const server = express();
 
 server.use(bodyParser.json({ limit: "30mb", exceeded: true}));
-server.use(bodyParser.urlencoded({ limit: "30mb", exceeded: true }));
+server.use(bodyParser.urlencoded({ limit: "30mb", exceeded: true, extended:true  }));
 server.use(cors())
 
 server.get('/', (req, res) => {
@@ -24,6 +24,17 @@ server.use('/posts', postRoutes);
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.eventNames.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => server.listen(PORT, () => console.log(`Mongo Connection successful. Server running on port: ${PORT}`)))
-    .catch((error) => console.log(`${error}: did not connect`))
+server.listen(PORT, () => console.log(` Server running on port: ${PORT}`))
+
+try {
+    await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Mongo Connection successful')
+    // server.listen(PORT, () => console.log(`Mongo Connection successful. Server running on port: ${PORT}`))
+} catch  (error) {
+    console.log(`${error}:did not connect`)
+}
+
+
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => server.listen(PORT, () => console.log(`Mongo Connection successful. Server running on port: ${PORT}`)))
+//     .catch((error) => console.log(`${error}: did not connect`))
